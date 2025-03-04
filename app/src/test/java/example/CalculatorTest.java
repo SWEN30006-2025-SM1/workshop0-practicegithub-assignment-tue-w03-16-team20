b5d25cffbb15946e509931ffabb0b71100aa2f07
Ben Zhou <you@example.com>
2025-03-04 17:38:05 +1100

    test change

--- app/src/test/java/example/CalculatorTest.java
+++ app/src/test/java/example/CalculatorTest.java
@@ -11,18 +11,18 @@ public class CalculatorTest {
     }
     @Test
     public void testAddBasic() {
-        Assert.assertEquals(4, calculator.add(1, 3));
+        Assert.assertEquals(5, calculator.add(2, 3));
     }
     @Test
     public void testMinusBasic() {
-        Assert.assertEquals(10, calculator.minus(15, 5));
+        Assert.assertEquals(15, calculator.minus(20, 5));
     }
     @Test
     public void testMultiplyBasic() {
-        Assert.assertEquals(10, calculator.multiply(2, 5));
+        Assert.assertEquals(16, calculator.multiply(2, 8));
     }
     @Test
     public void testDivideBasic() {
-        Assert.assertEquals(5, calculator.divide(10, 2), 0.5);
+        Assert.assertEquals(10, calculator.divide(20, 2), 0.5);
     }
 }
